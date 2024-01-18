c7ab5f1262de050cfb9bc6a7bcecba4d2e1fb894
KiprotichKibor <kiborotich24@gmail.com>
2024-01-18 09:01:10 -0800

    more changes on monty

--- pchar.c
+++ pchar.c
@@ -11,18 +11,22 @@ void f_pchar(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 
-	if (!head || !*head)
+	h = *head;
+	if (!h)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
-		clean_exit(*head);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
 	}
-
-	h = *head;
 	if (h->n > 127 || h->n < 0)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
-		clean_exit(*head);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
 	}
-
 	printf("%c\n", h->n);
 }
--- pint.c
+++ pint.c
@@ -1,18 +1,19 @@
 #include "monty.h"
-
 /**
- * f_pint - prints the top element of the stack
+ * f_pint - prints the element at the top of stack
  * @head: stack head
  * @counter: line_number
  * Return: no return
- */
+*/
 void f_pint(stack_t **head, unsigned int counter)
 {
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
-		clean_exit(*head);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
 	}
-
 	printf("%d\n", (*head)->n);
 }
